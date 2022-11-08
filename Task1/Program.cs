75b46318a7d2aa13df46b71482155869fe0caf69
Anatolii Yarmolenko <ipz224_yaam@student.ztu.edu.ua>
2022-11-08 23:46:58 +0200

    task18

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine("Task17");
+Console.WriteLine("Task17");
+Console.WriteLine("Task17");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
